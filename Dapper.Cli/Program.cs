1edacfd4ba155d9ef31a5185c80a148841a93ef9
Camilo Reyes <reyesc@comcast.net>
2022-01-17 17:30:14 -0600

    Do not avoid synchronization in main UI thread

--- Dapper.Cli/Program.cs
+++ Dapper.Cli/Program.cs
@@ -4,54 +4,36 @@ using Dapper.Data.Connection;
 var conn = new ConnectionProvider();
 var repo = new AdventureWorksRepository(conn);
 
-var executeResult = await repo
-  .UpdateSalesOrder(0, 5)
-  .ConfigureAwait(false);
+var executeResult = await repo.UpdateSalesOrder(0, 5);
 Console.WriteLine("Execute # of rows = " + executeResult);
 
-var queryResult = await repo
-  .GetSalesOrders()
-  .ConfigureAwait(false);
+var queryResult = await repo.GetSalesOrders();
 Console.WriteLine("Query # of rows = " + queryResult.Count);
 
-var queryFirstResult = await repo
-  .GetFirstSalesOrder()
-  .ConfigureAwait(false);
+var queryFirstResult = await repo.GetFirstSalesOrder();
 Console.WriteLine("QueryFirst SalesOrderId = " +
   queryFirstResult.SalesOrderId);
 
-var queryFirstOrDefaultResult = await repo
-  .GetFirstSalesOrderOrDefault(29825)
-  .ConfigureAwait(false);
+var queryFirstOrDefaultResult = await repo.GetFirstSalesOrderOrDefault(29825);
 Console.WriteLine("QueryFirstOrDefault SalesOrderId = " +
   queryFirstOrDefaultResult?.SalesOrderId);
 
-var querySingleResult = await repo
-  .GetSingleSalesOrder(43659)
-  .ConfigureAwait(false);
+var querySingleResult = await repo.GetSingleSalesOrder(43659);
 Console.WriteLine("QuerySingle SalesOrderId = " +
   querySingleResult.SalesOrderId);
 
-var querySingleOrDefaultResult = await repo
-  .GetSingleSalesOrderOrDefault(0)
-  .ConfigureAwait(false);
+var querySingleOrDefaultResult = await repo.GetSingleSalesOrderOrDefault(0);
 Console.WriteLine("QuerySingleOrDefault SalesOrderId = " +
   querySingleOrDefaultResult?.SalesOrderId);
 
-var queryMultipleResult = await repo
-  .GetSalesOrders(29825)
-  .ConfigureAwait(false);
+var queryMultipleResult = await repo.GetSalesOrders(29825);
 Console.WriteLine("QueryMultiple # of rows = " + queryMultipleResult.Count);
 
-var queryStoredProcedureResult = await repo
-  .GetEmployeeManagers(9)
-  .ConfigureAwait(false);
+var queryStoredProcedureResult = await repo.GetEmployeeManagers(9);
 Console.WriteLine("QueryStoredProcedure # of rows = " + queryStoredProcedureResult.Count);
 
 await repo.CreateSalesOrderTvp();
 Console.WriteLine("Created dbo.SalesOrderType TVP");
 
-var queryTvpResult = await repo
-  .GetSalesOrdersTvp(queryResult)
-  .ConfigureAwait(false);
+var queryTvpResult = await repo.GetSalesOrdersTvp(queryResult);
 Console.WriteLine("QueryTvp # of rows = " + queryTvpResult.Count);
